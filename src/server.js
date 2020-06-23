f5add0b928fbf6a96e5f03e8eb9747ebb1d5e78e
Phillipe Roger Souza <phillipe.souza@rte.com.br>
2020-06-22 22:12:04 -0300

    Habilitando o cors

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-const { Socket } = require('net');
+const cors = require('cors');
 
 const app = express();
 
@@ -9,7 +9,6 @@ app.use(cors());
 
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
-const cors = require('cors');
 
 
 const port = 3000;
